f6651e6a2b663acb1da5b9f7cfabeb147ea276b0
JD-Profe <jdr958@educa.madrid.org>
2025-06-30 19:21:31 +0200

    Últimos cambios: comentarios en .yml

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
+# Añadimos un fichero conftest.py al proyecto para configurar y compartir recursos comunes entre pruebas en pytest, 
+# sin necesidad de importar manualmente esos recursos en cada archivo de test.
+
 import sys
 import os
 
 # Añade la ruta absoluta del directorio 'app' al PYTHONPATH en tiempo de ejecución
 # Esto permite que los módulos dentro de 'app' sean importados correctamente durante las pruebas.
-# Importamos pytest para poder usar sus funcionalidades de pruebas
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "app")))
